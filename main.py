e9a38422beaf596fae5de3c4686abdaf14cdbded
Shengwei-Peng <m11207330@mail.ntust.edu.tw>
2024-10-10 21:25:03 +0800

    feat: implement new Trainer pipeline

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 """main"""
-from src import parse_arguments, set_seed, Trainer
+from src import parse_arguments, Trainer
 
 
 def main() -> None:
     """main"""
     args = parse_arguments()
-    set_seed(args["random_state"])
     trainer = Trainer(**args)
-    trainer.pre_process()
     trainer.train()
 
 if __name__ == "__main__":
--- src/trainer.py
+++ src/trainer.py
@@ -1,6 +1,6 @@
 """trainer"""
 from pathlib import Path
-from typing import Dict, Any, Tuple
+from typing import Dict, Any
 
 import numpy as np
 import pandas as pd
@@ -15,137 +15,79 @@ from ngboost import NGBRegressor
 from pytorch_tabnet.tab_model import TabNetRegressor
 
 from .models import MLPRegressor
+from .utils import set_seed, get_dataset
 
 class Trainer:
     """Trainer"""
     def __init__(
         self,
         data_folder: Path,
-        combine_data: bool = True,
-        target_column: str = "Power(mW)",
-        test_start_time: str = "09:00:00",
-        test_end_time: str = "16:59:00",
-        window_size: int = 10,
-        test_days: int = 200,
+        look_back_steps: int = 12,
+        n_test_months: int = 2,
         random_state: int = 42,
+        combine_data: bool = True,
     ) -> None:
-        self.dataset: list = []
-        self.data_folder = data_folder
-        self.combine_data = combine_data
-        self.target_column = target_column
-        self.window_size = window_size
-        self.test_start_time = pd.to_datetime(test_start_time).time()
-        self.test_end_time = pd.to_datetime(test_end_time).time()
-        self.test_days = pd.Timedelta(days=test_days)
-        self.random_state = random_state
+        set_seed(random_state)
+        self.dataset = get_dataset(data_folder, look_back_steps, n_test_months, combine_data)
         self.models = {
             "Linear Regression": LinearRegression(),
             "XGBoost": XGBRegressor(
-                n_jobs=-1, random_state=self.random_state
+                n_jobs=-1, random_state=random_state
             ),
             "LightGBM": LGBMRegressor(
-                verbose=-1, n_jobs=-1, random_state=self.random_state
+                verbose=-1, n_jobs=-1, random_state=random_state
             ),
             "CatBoost": CatBoostRegressor(
-                verbose=0, thread_count=-1, random_state=self.random_state
+                verbose=0, thread_count=-1, random_state=random_state
             ),
             "NGBoost": NGBRegressor(
-                verbose=False, random_state=self.random_state
+                verbose=False, random_state=random_state
             ),
             "TabNet": TabNetRegressor(),
             "MLP": MLPRegressor(),
         }
 
-    def pre_process(self) -> None:
-        """pre_process"""
-        for i, csv_file in enumerate(self.data_folder.glob("*.csv")):
-            data = self.load_data(csv_file)
-            if self.combine_data:
-                if i == 0:
-                    combined_data = data
-                else:
-                    for split in ["train", "test"]:
-                        combined_data[split]["X"] = np.concatenate(
-                            [combined_data[split]["X"], data[split]["X"]], axis=0
-                        )
-                        combined_data[split]["y"] = np.concatenate(
-                            [combined_data[split]["y"], data[split]["y"]], axis=0
-                        )
-            else:
-                self.dataset.append({
-                    "file_name": csv_file.name,
-                    "train": data["train"],
-                    "test": data["test"]
-                })
+    def train(self) -> None:
+        """train"""
+        results = []
+        for data in self.dataset:
+            data_dimensions = [
+                ["Train X", data["regression"]["train"]["x"].shape],
+                ["Train Y", data["regression"]["train"]["y"].shape],
+                ["Valid X", data["regression"]["valid"]["x"].shape],
+                ["Valid Y", data["regression"]["valid"]["y"].shape]
+            ]
+            print("\nData Dimensions:")
+            print(tabulate(data_dimensions, headers=["Data", "Shape"], tablefmt="pretty"))
 
-        if self.combine_data:
-            self.dataset.append({
-                "file_name": "Combined Data",
-                "train": combined_data["train"],
-                "test": combined_data["test"]
+            result = self._find_best_model(data["regression"])
+            results.append({
+                "File": data["file_name"],
+                "Best Model": result["model_name"],
+                "MAE": result["metrics"]["MAE"],
+                "MSE": result["metrics"]["MSE"],
+                "RMSE": result["metrics"]["RMSE"],
+                "R² Score": result["metrics"]["R² Score"]
             })
 
-    def load_data(self, file_path: str) -> Dict[str, Dict[str, np.ndarray]]:
-        """Load and preprocess data."""
-        raw_data = pd.read_csv(file_path)
-        raw_data["DateTime"] = pd.to_datetime(raw_data["DateTime"])
+        results_df = pd.DataFrame(results)
+        average_metrics = results_df.mean(numeric_only=True).to_dict()
 
-        max_date = raw_data["DateTime"].max()
-        min_test_date = max_date - self.test_days
-        is_in_test_days = raw_data["DateTime"] >= min_test_date
-        is_in_time_range = raw_data["DateTime"].dt.time.between(
-            self.test_start_time, self.test_end_time
-        )
-        test_data = raw_data[is_in_test_days & is_in_time_range].copy()
-        train_data = raw_data[~(is_in_test_days & is_in_time_range)].copy()
+        average_row = pd.DataFrame([{
+            "File": "Average",
+            "Best Model": "-",
+            "MAE": round(average_metrics["MAE"], 4),
+            "MSE": round(average_metrics["MSE"], 1),
+            "RMSE": round(average_metrics["RMSE"], 4),
+            "R² Score": round(average_metrics["R² Score"], 4)
+        }])
+        results_df = pd.concat([results_df, average_row], ignore_index=True)
+        results_df = results_df.sort_values(by="MAE", ascending=True)
 
-        for df in [train_data, test_data]:
-            df.loc[:, "month"] = df["DateTime"].dt.month
-            df.loc[:, "day"] = df["DateTime"].dt.day
-            df.loc[:, "hour"] = df["DateTime"].dt.hour
-            df.loc[:, "minute"] = df["DateTime"].dt.minute
-            df.loc[:, "second"] = df["DateTime"].dt.second
+        print("\nModel Performance Across Datasets:")
+        print(tabulate(results_df, headers="keys", tablefmt="pretty", showindex=False))
 
-        train_x = train_data.drop(columns=[self.target_column, "DateTime"]).values
-        train_y = train_data[self.target_column].values
-
-        test_x = test_data.drop(columns=[self.target_column, "DateTime"]).values
-        test_y = test_data[self.target_column].values
-
-        train_x, train_y = self.sliding_window(train_x, train_y)
-        test_x, test_y = self.sliding_window(test_x, test_y)
-
-        return {
-            "train": {
-                "X": train_x,
-                "y": train_y.reshape(-1, 1)
-            },
-            "test": {
-                "X": test_x,
-                "y": test_y.reshape(-1, 1)
-            }
-        }
-
-    def sliding_window(
-        self,
-        features: np.ndarray,
-        target: np.ndarray
-        ) -> Tuple[np.ndarray, np.ndarray]:
-        """sliding_window"""
-
-        num_samples = len(features) - self.window_size
-        x = np.empty((num_samples, self.window_size * features.shape[1]))
-        y = np.empty(num_samples)
-
-        for i in range(num_samples):
-            x[i] = features[i:i + self.window_size].flatten()
-            y[i] = target[i + self.window_size - 1]
-
-        return x, y
-
-    def find_best_model(self, data: Dict[str, Dict[str, pd.DataFrame]]) -> Dict[str, Any]:
-        """find_best_model"""
-
+    def _find_best_model(self, data: Dict[str, Dict[str, pd.DataFrame]]) -> Dict[str, Any]:
         best_info = {
             "model": None,
             "name": "",
@@ -158,9 +100,9 @@ class Trainer:
             for name, model in pbar:
                 pbar.set_description(f"Training {name}")
 
-                model.fit(data["train"]["X"], data["train"]["y"])
-                y_pred = model.predict(data["test"]["X"])
-                y_true = data["test"]["y"]
+                model.fit(data["train"]["x"], data["train"]["y"])
+                y_pred = model.predict(data["valid"]["x"])
+                y_true = data["valid"]["y"]
 
                 metrics = {
                     "MAE": mean_absolute_error(y_true, y_pred),
@@ -192,43 +134,3 @@ class Trainer:
             "model": best_info["model"],
             "metrics": best_info["metrics"]
         }
-
-    def train(self) -> None:
-        """train"""
-        results = []
-        for data in self.dataset:
-            data_dimensions = [
-                ["Training data (X)", data['train']['X'].shape],
-                ["Training target (y)", data['train']['y'].shape],
-                ["Testing data (X)", data['test']['X'].shape],
-                ["Testing target (y)", data['test']['y'].shape]
-            ]
-            print("\nData Dimensions:")
-            print(tabulate(data_dimensions, headers=["Data", "Shape"], tablefmt="pretty"))
-
-            result = self.find_best_model(data)
-            results.append({
-                "File": data["file_name"],
-                "Best Model": result["model_name"],
-                "MAE": result["metrics"]["MAE"],
-                "MSE": result["metrics"]["MSE"],
-                "RMSE": result["metrics"]["RMSE"],
-                "R² Score": result["metrics"]["R² Score"]
-            })
-
-        results_df = pd.DataFrame(results)
-        average_metrics = results_df.mean(numeric_only=True).to_dict()
-
-        average_row = pd.DataFrame([{
-            "File": "Average",
-            "Best Model": "-",
-            "MAE": round(average_metrics["MAE"], 4),
-            "MSE": round(average_metrics["MSE"], 1),
-            "RMSE": round(average_metrics["RMSE"], 4),
-            "R² Score": round(average_metrics["R² Score"], 4)
-        }])
-        results_df = pd.concat([results_df, average_row], ignore_index=True)
-        results_df = results_df.sort_values(by="MAE", ascending=True)
-
-        print("\nModel Performance Across Datasets:")
-        print(tabulate(results_df, headers="keys", tablefmt="pretty", showindex=False))
